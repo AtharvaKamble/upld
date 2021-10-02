1b786d7190962bc8f6cbb1af34665b29c904857e
AtharvaKamble <atharvadkamble00@gmail.com>
2021-10-02 14:59:10 +0530

    feat: add single blog upload for tumblr

--- src/tumblr/index.js
+++ src/tumblr/index.js
@@ -1,16 +1,37 @@
 import TClient from "tumblr.js";
 import Credentials from "../../credentials.json";
 
-const { OAuth_consumer_key, OAuth_consumer_secret, token, token_secret } =
-  Credentials.Tumblr;
+const {
+  OAuth_consumer_key,
+  OAuth_consumer_secret,
+  token,
+  token_secret,
+  blogIdentifier,
+} = Credentials.Tumblr;
 
-function tumblr({ csvPath, title, content }) {
-  const client = tumblr.createClient({
-    consumer_key: OAuth_consumer_key,
-    consumer_secret: OAuth_consumer_secret,
-    token: token,
-    token_secret: token_secret,
+async function tumblr({ csvPath, title, content }) {
+  const client = await TClient.createClient({
+    credentials: {
+      consumer_key: OAuth_consumer_key,
+      consumer_secret: OAuth_consumer_secret,
+      token: token,
+      token_secret: token_secret,
+    },
+    returnPromises: true,
   });
+
+  const blogDetails = {
+    title: "Hello from Node.js, Part B",
+    body: "<h1>This is done programmatically, yooohooo!</h1>",
+    state: "draft",
+    source: "https://raftlabs.co",
+  };
+
+  const res = await client.createTextPost(blogIdentifier, blogDetails);
+
+  if (res.id) {
+    console.log(res);
+  }
 }
 
 export default tumblr;
